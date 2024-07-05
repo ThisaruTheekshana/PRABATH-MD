a4875fd91ba3ca824e19b5043f6b61f1086c962c
ThisaruTheekshana <Thisaruthikshana9@gmail.com>
2024-07-05 20:18:04 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
 const username = fs.readFileSync('github_username.txt', 'utf8').trim()
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,PRABATH-MD~aWpBHTqJ#ZG4-PK9JTA0DyQrEfW9YNwr8aUyhAItgGo3_prS6EkQ    
+    SESSION_ID:  process.env.SESSION_ID,PRABATH-MD~aWpBHTqJ#ZG4-PRABATH-MD~cit2SSAS#nnUUnIU99a32vHnWbjlovmnCBJN_Cld9dvOgUvOYsWc   
     BOT_NUMBER:  process.env.BOT_NUMBER,+94 70 468 3763
     GITHUB_USERNAME: username,theekshana
     GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN_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
